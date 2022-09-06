b3bf087053227cd6eff96dc5dc04c35e9c864144
Carol Martins <carolmartins09@gmail.com>
2022-09-06 18:19:15 -0300

    exemplos da aula de tipos primitivos

---
+++ type-string.js
@@ -0,0 +1,12 @@
+const texto1 = "Olá, mundo!";
+const texto2 ='Olá, mundo!';
+const senha = "senhasegura123*"
+const stringDeNumeros = "34567";
+const citacao = 'Meu nome é ';
+const meuNome = "Leonardo"
+
+// template string OU template literal
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
